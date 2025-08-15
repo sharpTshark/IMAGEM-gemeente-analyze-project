5eb2965f5d75c685a64f4d14116b4a65fe4c6540
sharpTshark <71552553+sharpTshark@users.noreply.github.com>
2025-08-15 11:16:20 +0200

    init charts library

---
+++ app/components/chart.vue
@@ -0,0 +1,36 @@
+<template>
+	<canvas :id="id" ></canvas>
+</template>
+
+<script>
+
+import * as echarts from "echarts";
+
+export default {
+	name: "ChartComponent",
+	data() {
+		return {
+			chart: null
+		}
+	},	
+	props: {
+		id: {
+			type: String,
+			required: true
+		},
+		chartOptions: {
+			type: Object,
+			default: {}
+		}
+	},
+	mounted() {
+
+		const chartElement = document.getElementById(this.id)
+
+		this.chart = echarts.init(chartElement)
+
+		this.chart.setOption(this.chartOptions)
+	}
+};
+
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,35 @@
 <template>
 	<div>
-		Index
+		<Chart id="1" :chart-options="chartOptions" />
 	</div>
 </template>
+
+
+<script>
+
+export default {
+	name: "index",
+	data() {
+		return {
+			chartOptions: {
+				title: {
+					text: "ECharts Getting Started Example"
+				},
+				tooltip: {},
+				xAxis: {
+					data: ["shirt", "cardigan", "chiffon", "pants", "heels", "socks"]
+				},
+				yAxis: {},
+				series: [
+					{
+					name: "sales",
+					type: "bar",
+					data: [5, 20, 36, 10, 10, 20]
+					}
+				]
+			}
+		}
+	}
+}
+
+</script>
